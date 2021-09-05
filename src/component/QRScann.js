0766a564995e88a96c84d8bdcb84d2871ca2d323
Juan M Garcia <garcia_juanm@hotmail.com>
2021-09-05 09:54:35 -0300

    Modifica btn en column

--- src/component/QRScann.js
+++ src/component/QRScann.js
@@ -12,24 +12,15 @@ const useStyles = makeStyles((theme) => ({
   conatiner: {
     marginTop: 10
   },
-  title: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems:  'center',
-    background: '#3f51b5',
-    color: '#fff',
-    padding: 20
-  },
   title1:{
     [theme.breakpoints.up('xs')]: {
       display: 'flex',
       justifyContent: 'center',
       alignItems:  'center',
       background: '#3f51b5',
-      // background: '#fb61bb',
       color: '#fff',
       padding: 10,
-      width: '80%',
+      width: '100%',
       height: '80%',
       fontSize: `calc(6px + 2vmin)`
       // width: `calc(40% - ${100}px)`
@@ -81,10 +72,12 @@ export const QRScann = () => {
             {/* <Propiedades /> */}
             <Card>
                 <Link to={`./`}>
-                    <h2 className={classes.title1}>Apuntar la etiqueta sobre la camara y espere el resultado</h2>
+                    <h2 className={classes.title1}>
+                        Apuntar la etiqueta sobre la camara y espere el resultado
+                    </h2>
                 </Link>
-                <CardContent>
-                    <Grid container spacing={2} direction="column">
+                <CardContent xs={12}>
+                    <Grid>
                         <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
                            <h3
                               className= "titulos"
@@ -92,13 +85,14 @@ export const QRScann = () => {
                              Ajuste el codigo QR en el recuadro</h3>
                            <QrReader
                               delay={300}
-                              style={{width: '100%'}}
+                              // style={{width: '100%'}}
                               // style={myStyle}
                               onError={handleErrorWebCam}
                               onScan={handleScanWebCam}
                               // className="qrcode"
                            />
-                          <h3 
+                        </Grid>
+                        <h3 
                             className= "titulos" 
                           >
                               Resultado WebCam -  {scanResultWebCam}</h3>
@@ -112,7 +106,6 @@ export const QRScann = () => {
                             Ejecutar resultado
                           </Button>
 
-                        </Grid>
                     </Grid>
                 </CardContent>
             </Card>
